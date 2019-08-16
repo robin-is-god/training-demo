08566cd14b227af3cb727c73c80a06aa242d5b12
annliao <1530560200@qq.com>
2019-08-16 14:40:56 +0800

    push orderRsp again and again

---
+++ src/main/java/com/spdb/training/beans/order/OrderRsp.java
@@ -0,0 +1,16 @@
+package com.spdb.training.beans.order;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName: OrderRsp
+ * @Description: TODO:
+ * @author annliao
+ * @date 2019年8月16日
+ *
+ */
+public class OrderRsp implements Serializable{
+	private static final long serialVersionUID = -1017085371735723853L;
+	//购买商品返回报文体为空,暂不实现此类！
+
+}
