2b8a15f5c7d7dafe3cb27b65fa48598bf38cd385
annliao <1530560200@qq.com>
2019-08-16 16:12:40 +0800

    update OrderService

---
+++ src/main/java/com/spdb/training/services/order/OrderService.java
@@ -0,0 +1,16 @@
+package com.spdb.training.services.order;
+
+import com.spdb.training.beans.order.Order;
+
+/**
+ * @ClassName:OrderService
+ * @Descritpion:TODO:
+ * @author annliao
+ * @date:2019年8月16日
+ *
+ */
+public interface OrderService {
+	//购买增加一条记录
+	void insert(Order order);
+	
+}
